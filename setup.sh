6216090230028d6162dd9179ac1d3506dd6741d8
Josiah-MCS <163807283+Josiah-MCS@users.noreply.github.com>
2024-07-16 09:09:25 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ function clone_configs() {
     git clone https://github.com/Josiah-tan/vimconfig.git ~/.vim
     git clone https://github.com/Josiah-tan/nvimconfig.git ~/.config/nvim
     git clone https://github.com/Josiah-tan/quick-projects-nvim-template.git ~/.config/.quick_projects
-    curl -o ~/.tmux.conf https://raw.githubusercontent.com/Josiah-tan/.dotfiles/main/tmux/.tmux.conf?token=GHSAT0AAAAAACPXAEXPBENFTV5PZBI53Z4SZUUWXOQ
+    curl -o ~/.tmux.conf https://raw.githubusercontent.com/Josiah-tan/.dotfiles/main/tmux/.tmux.conf
 }
 
 show_help() {
